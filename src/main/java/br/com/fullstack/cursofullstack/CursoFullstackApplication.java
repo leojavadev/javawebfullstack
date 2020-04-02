38dd6ff740d41efddf64b9c0fef9767f4ea738fb
leojavadev <leojavadev@gmail.com>
2020-04-02 10:50:57 -0300

    Projeto criado

---
+++ src/main/java/br/com/fullstack/cursofullstack/CursoFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.cursofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstack/cursofullstack/CursoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.cursofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
