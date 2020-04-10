6861353334f7aca5b1a54930e4c0e75610fce3ce
leojavadev <leojavadev@gmail.com>
2020-04-10 19:10:02 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/br/com/fullstack/cursofullstack/resources/CategoriaResource.java
+++ src/main/java/br/com/fullstack/cursofullstack/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.fullstack.cursofullstack.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fullstack.cursofullstack.domain.Categoria;
+
 @RestController
-@RequestMapping(path = "/teste")
+@RequestMapping(path = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String teste() {
-		return "O teste funcionou!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
