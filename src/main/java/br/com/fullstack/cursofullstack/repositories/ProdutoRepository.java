3562dd27a5c511c0d0ed840579d944d5e05883a3
leojavadev <leojavadev@gmail.com>
2020-04-11 19:37:48 -0300

    Implementação da classe Produto, do repositório e da associação com a classe Categoria

--- src/main/java/br/com/fullstack/cursofullstack/repositories/ProdutoRepository.java
+++ src/main/java/br/com/fullstack/cursofullstack/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package br.com.fullstack.cursofullstack.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.fullstack.cursofullstack.domain.Categoria;
+import br.com.fullstack.cursofullstack.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
