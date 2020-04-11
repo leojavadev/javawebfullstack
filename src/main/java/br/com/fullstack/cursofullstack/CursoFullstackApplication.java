80fc4de4ecb48a8f74d39ebea8a6a6d2801ceabc
leojavadev <leojavadev@gmail.com>
2020-04-11 17:57:47 -0300

    Inserindo dados automaticamente na inicialização da app

--- src/main/java/br/com/fullstack/cursofullstack/CursoFullstackApplication.java
+++ src/main/java/br/com/fullstack/cursofullstack/CursoFullstackApplication.java
@@ -1,13 +1,30 @@
 package br.com.fullstack.cursofullstack;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoFullstackApplication {
+import br.com.fullstack.cursofullstack.domain.Categoria;
+import br.com.fullstack.cursofullstack.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoFullstackApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoFullstackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
