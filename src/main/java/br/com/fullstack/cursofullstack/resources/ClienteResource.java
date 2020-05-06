a9699aa6a2130a6004f63a59ec14ec9b4427bcb5
leojavadev <leojavadev@gmail.com>
2020-05-05 21:22:09 -0300

    CRUD da entidade Cliente, com validação e paginação

--- src/main/java/br/com/fullstack/cursofullstack/resources/ClienteResource.java
+++ src/main/java/br/com/fullstack/cursofullstack/resources/ClienteResource.java
@@ -1,15 +1,24 @@
 package br.com.fullstack.cursofullstack.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.fullstack.cursofullstack.domain.Cliente;
+import br.com.fullstack.cursofullstack.dto.ClienteDTO;
 import br.com.fullstack.cursofullstack.services.ClienteService;
 
 @RestController()
@@ -29,5 +38,39 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> findById(@PathVariable Integer id){
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
+		Cliente obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody ClienteDTO objDto){
+		Cliente obj = new Cliente(id, objDto.getNome(), objDto.getEmail(), null, null);
+		obj = service.update(obj);
+		return ResponseEntity.ok().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> page(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy
+		){
+		Page<Cliente> lista = service.findPage(page, linesPerPage, direction, orderBy);
+		Page<ClienteDTO> listaDto = lista.map(x -> new ClienteDTO(x));
+		return ResponseEntity.ok().body(listaDto);
 	}
 }
--- src/main/java/br/com/fullstack/cursofullstack/services/ClienteService.java
+++ src/main/java/br/com/fullstack/cursofullstack/services/ClienteService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.fullstack.cursofullstack.domain.Cliente;
+import br.com.fullstack.cursofullstack.dto.ClienteDTO;
 import br.com.fullstack.cursofullstack.repositories.ClienteRepository;
+import br.com.fullstack.cursofullstack.services.exceptions.DataIntegrityException;
 import br.com.fullstack.cursofullstack.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +36,38 @@ public class ClienteService {
 				() -> new ObjectNotFoundException("Cliente não encontrado com o ID informado: ID = " + id + ", Tipo: " + Cliente.class.getName())
 		);
 	}
-
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente fromDto(ClienteDTO obj) {
+		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null);
+	}
+	
+	public Cliente update(Cliente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+				"Para apagar um cliente, é necessário excluir, primeiro,"
+				+ " os seus pedidos, telefone e endereço"
+			);
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(
+			page, linesPerPage, Direction.valueOf(direction), orderBy
+		);
+		return repo.findAll(pageRequest);
+	}
 }
