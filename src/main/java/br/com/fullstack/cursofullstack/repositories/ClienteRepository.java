b4c582c9e6389fafc1b4a71ff184bfe8b3141a8a
leojavadev <leojavadev@gmail.com>
2020-05-09 21:05:06 -0300

    Validação de e-mail já cadastrado personalizando o ClienteRepository

--- src/main/java/br/com/fullstack/cursofullstack/repositories/ClienteRepository.java
+++ src/main/java/br/com/fullstack/cursofullstack/repositories/ClienteRepository.java
@@ -1,9 +1,13 @@
 package br.com.fullstack.cursofullstack.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.fullstack.cursofullstack.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/br/com/fullstack/cursofullstack/services/validation/ClientInsertValidator.java
+++ src/main/java/br/com/fullstack/cursofullstack/services/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.fullstack.cursofullstack.domain.Cliente;
 import br.com.fullstack.cursofullstack.domain.enums.TipoCliente;
 import br.com.fullstack.cursofullstack.dto.ClienteNewDTO;
+import br.com.fullstack.cursofullstack.repositories.ClienteRepository;
 import br.com.fullstack.cursofullstack.resources.exceptions.FieldMessage;
 import br.com.fullstack.cursofullstack.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClienteNewDTO>{
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClientInsert ann){}
@@ -27,6 +34,12 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+		}
+		
+		Cliente clienteEmail = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(clienteEmail != null){
+			list.add(new FieldMessage("email", "E-mail já cadastrado!"));
 		}
 		
 		for(FieldMessage e : list){
