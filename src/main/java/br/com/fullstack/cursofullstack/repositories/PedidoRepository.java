37ec44d5f329f9e1d7727bbe2c8266ef10155bd4
leojavadev <leojavadev@gmail.com>
2020-11-06 17:37:35 -0300

    Restrição de conteúdo - cliente só recupera seus pedidos

--- src/main/java/br/com/fullstack/cursofullstack/repositories/PedidoRepository.java
+++ src/main/java/br/com/fullstack/cursofullstack/repositories/PedidoRepository.java
@@ -1,9 +1,16 @@
 package br.com.fullstack.cursofullstack.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.fullstack.cursofullstack.domain.Cliente;
 import br.com.fullstack.cursofullstack.domain.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/br/com/fullstack/cursofullstack/services/PedidoService.java
+++ src/main/java/br/com/fullstack/cursofullstack/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.fullstack.cursofullstack.domain.Cliente;
 import br.com.fullstack.cursofullstack.domain.ItemPedido;
 import br.com.fullstack.cursofullstack.domain.PagamentoComBoleto;
 import br.com.fullstack.cursofullstack.domain.Pedido;
@@ -14,6 +18,8 @@ import br.com.fullstack.cursofullstack.domain.enums.EstadoPagamento;
 import br.com.fullstack.cursofullstack.repositories.ItemPedidoRepository;
 import br.com.fullstack.cursofullstack.repositories.PagamentoRepository;
 import br.com.fullstack.cursofullstack.repositories.PedidoRepository;
+import br.com.fullstack.cursofullstack.resources.exceptions.AuthorizationException;
+import br.com.fullstack.cursofullstack.security.UserSS;
 import br.com.fullstack.cursofullstack.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -80,5 +86,15 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
