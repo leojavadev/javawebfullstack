13e2201f8e0b0d74f21aa1fc42280db1798ccd65
leojavadev <leojavadev@gmail.com>
2020-12-28 19:13:54 -0300

    Arquivo de configuração do ambiente de produção

--- src/main/java/br/com/fullstack/cursofullstack/config/DevConfig.java
+++ src/main/java/br/com/fullstack/cursofullstack/config/DevConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import br.com.fullstack.cursofullstack.services.DBService;
 import br.com.fullstack.cursofullstack.services.EmailService;
